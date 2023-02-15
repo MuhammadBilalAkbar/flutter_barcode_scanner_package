d892023f5e84857647ef2281ac47a9d0fa28f76e
Muhammad Bilal Akbar <mbilalakbarkh786@gmail.com>
2023-02-15 16:52:16 +0500

    Initial commit

--- lib/main.dart
+++ lib/main.dart
@@ -6,15 +6,31 @@ import 'package:flutter_barcode_scanner/flutter_barcode_scanner.dart';
 
 void main() => runApp(const MyApp());
 
-class MyApp extends StatefulWidget {
+class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
   @override
-  MyAppState createState() => MyAppState();
+  Widget build(BuildContext context) => MaterialApp(
+        title: 'Barcode scan',
+        theme: ThemeData(
+          primarySwatch: Colors.blue,
+          textTheme: const TextTheme(
+            bodyMedium: TextStyle(fontSize: 24.0),
+          ),
+        ),
+        home: const BarCodeScanner(),
+      );
 }
 
-class MyAppState extends State<MyApp> {
-  String _scanBarcode = 'Unknown';
+class BarCodeScanner extends StatefulWidget {
+  const BarCodeScanner({super.key});
+
+  @override
+  BarCodeScannerState createState() => BarCodeScannerState();
+}
+
+class BarCodeScannerState extends State<BarCodeScanner> {
+  String _scanBarcodeResult = 'Unknown';
 
   @override
   void initState() {
@@ -28,11 +44,10 @@ class MyAppState extends State<MyApp> {
       true,
       ScanMode.BARCODE,
     )!
-        .listen(
-      (barcode) => debugPrint(barcode),
-    );
+        .listen((barcode) => debugPrint(barcode));
   }
 
+  // Platform messages are asynchronous, so we initialize in an async method.
   Future<void> scanQR() async {
     String barcodeScanRes;
     // Platform messages may fail, so we use a try/catch PlatformException.
@@ -54,14 +69,12 @@ class MyAppState extends State<MyApp> {
     if (!mounted) return;
 
     setState(() {
-      _scanBarcode = barcodeScanRes;
+      _scanBarcodeResult = barcodeScanRes;
     });
   }
 
-  // Platform messages are asynchronous, so we initialize in an async method.
   Future<void> scanBarcodeNormal() async {
     String barcodeScanRes;
-    // Platform messages may fail, so we use a try/catch PlatformException.
     try {
       barcodeScanRes = await FlutterBarcodeScanner.scanBarcode(
         '#ff6666',
@@ -74,13 +87,10 @@ class MyAppState extends State<MyApp> {
       barcodeScanRes = 'Failed to get platform version.';
     }
 
-    // If the widget was removed from the tree while the asynchronous platform
-    // message was in flight, we want to discard the reply rather than calling
-    // setState to update our non-existent appearance.
     if (!mounted) return;
 
     setState(() {
-      _scanBarcode = barcodeScanRes;
+      _scanBarcodeResult = barcodeScanRes;
     });
   }
 
@@ -88,36 +98,38 @@ class MyAppState extends State<MyApp> {
   Widget build(BuildContext context) => MaterialApp(
         home: Scaffold(
           appBar: AppBar(
-            title: const Text('Barcode scan'),
+            title: Text(
+              'Barcode scan',
+              style: TextStyle(
+                  fontSize: Theme.of(context).textTheme.bodyMedium!.fontSize),
+            ),
           ),
           body: Builder(
-            builder: (BuildContext context) {
-              return Container(
-                alignment: Alignment.center,
-                child: Flex(
-                  direction: Axis.vertical,
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: <Widget>[
-                    ElevatedButton(
-                      onPressed: () => scanBarcodeNormal(),
-                      child: const Text('Start barcode scan'),
-                    ),
-                    ElevatedButton(
-                      onPressed: () => scanQR(),
-                      child: const Text('Start QR scan'),
-                    ),
-                    ElevatedButton(
-                      onPressed: () => startBarcodeScanStream(),
-                      child: const Text('Start barcode scan stream'),
-                    ),
-                    Text(
-                      'Scan result : $_scanBarcode\n',
-                      style: const TextStyle(fontSize: 20),
-                    )
-                  ],
-                ),
-              );
-            },
+            builder: (context) => Container(
+              alignment: Alignment.center,
+              child: Flex(
+                direction: Axis.vertical,
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: <Widget>[
+                  ElevatedButton(
+                    onPressed: scanBarcodeNormal,
+                    child: const Text('Start barcode scan'),
+                  ),
+                  ElevatedButton(
+                    onPressed: scanQR,
+                    child: const Text('Start QR scan'),
+                  ),
+                  ElevatedButton(
+                    onPressed: startBarcodeScanStream,
+                    child: const Text('Start barcode scan stream'),
+                  ),
+                  Text(
+                    'Scan result : $_scanBarcodeResult\n',
+                    style: const TextStyle(fontSize: 20),
+                  )
+                ],
+              ),
+            ),
           ),
         ),
       );
